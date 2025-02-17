08cceb56dfe6d568fc5ebdd27e80e613e0b6df36
tak <re795h@gmail.com>
2025-02-17 12:57:03 +0900

    fix cast -j option error

--- scripts/check-replica.sh
+++ scripts/check-replica.sh
@@ -7,13 +7,13 @@ set -eu
 BLOCK="${1:-latest}"
 
 while true; do
-  origin_number="$(cast block -j --rpc-url $ORIGIN_RPC_URL $BLOCK | jq -rM .number)"
-  origin_hash="$(cast   block -j --rpc-url $ORIGIN_RPC_URL $BLOCK | jq -rM .hash)"
-  origin_state="$(cast  block -j --rpc-url $ORIGIN_RPC_URL $BLOCK | jq -rM .stateRoot)"
+  origin_number="$(cast block --json --rpc-url $ORIGIN_RPC_URL $BLOCK | jq -rM .number)"
+  origin_hash="$(cast   block --json --rpc-url $ORIGIN_RPC_URL $BLOCK | jq -rM .hash)"
+  origin_state="$(cast  block --json --rpc-url $ORIGIN_RPC_URL $BLOCK | jq -rM .stateRoot)"
 
-  replica_number="$(cast block -j --rpc-url $REPLICA_RPC_URL $BLOCK | jq -rM .number)"
-  replica_hash="$(cast   block -j --rpc-url $REPLICA_RPC_URL $BLOCK | jq -rM .hash)"
-  replica_state="$(cast  block -j --rpc-url $REPLICA_RPC_URL $BLOCK | jq -rM .stateRoot)"
+  replica_number="$(cast block --json --rpc-url $REPLICA_RPC_URL $BLOCK | jq -rM .number)"
+  replica_hash="$(cast   block --json --rpc-url $REPLICA_RPC_URL $BLOCK | jq -rM .hash)"
+  replica_state="$(cast  block --json --rpc-url $REPLICA_RPC_URL $BLOCK | jq -rM .stateRoot)"
 
   if [ x"$origin_number" == x -o x"$replica_number" == x ]; then
     echo "Error: Failed to retrieve latest block" >&2
